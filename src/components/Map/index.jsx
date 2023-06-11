7528fd0c18e84431ebe4a1ed2bb60c76b6610bc4
Abrorbek abdulkhamidov <abdulkhamidovabrorbek@gmail.com>
2023-06-11 16:04:44 +0500

    fixed

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,10 +1,7 @@
-import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { useCallback, useMemo, useRef, useState } from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import L from "leaflet";
-// import "leaflet/dist/leaflet/leaflet.css";
-// import 'leaflet/dist/leaflet.css';
 
-// console.log(L)
 const center = {
   lat: 51.505,
   lng: -0.09,
@@ -76,68 +73,5 @@ function MapWrapper() {
     </MapContainer>
   );
 }
-
-
-
-// const center = {
-//   lat: 51.505,
-//   lng: -0.09,
-// };
-
-// function DraggableMarker() {
-//   const [draggable, setDraggable] = useState(true);
-//   const [position, setPosition] = useState(center);
-//   const markerRef = useRef(null);
-//   const eventHandlers = useMemo(
-//     () => ({
-//       dragend() {
-//         const marker = markerRef.current;
-//         if (marker != null) {
-//           setPosition(marker.getBounds().getCenter());
-//         }
-//       },
-//     }),
-//     []
-//   );
-//   const toggleDraggable = useCallback(() => {
-//     setDraggable((d) => !d);
-//   }, []);
-
-//   useEffect(() => {
-//     console.log("Position:", position);
-//   }, [position]);
-
-//   return (
-//     <Marker
-//     icon={markerIcon}
-//       draggable={draggable}
-//       eventHandlers={eventHandlers}
-//       position={position}
-//       ref={markerRef}
-//     >
-//       <Popup minWidth={90}>
-//         <span onClick={toggleDraggable}>
-//           {draggable
-//             ? "Marker is draggable"
-//             : "Click here to make marker draggable"}
-//         </span>
-//       </Popup>
-//     </Marker>
-//   );
-// }
-
-// const MapWrapper = () => {
-//   return (
-//     <div className="mapview" style={{ width: "100%", height: "450px" }}>
-//       <MapContainer center={center} zoom={6} scrollWheelZoom={false}>
-//         <TileLayer
-//           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-//         />
-//         <DraggableMarker />
-//       </MapContainer>
-//     </div>
-//   );
-// };
 
 export default MapWrapper;
