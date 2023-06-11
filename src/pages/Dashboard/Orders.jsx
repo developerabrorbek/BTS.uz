6ac0a71ae4dc1ce262b9231d93bf91d4855b2820
BerdiqulovN-dev <navruzbekturalov0333@gmail.com>
2023-06-11 21:29:38 +0500

    orders added and fixed

--- src/pages/Dashboard/Orders.jsx
+++ src/pages/Dashboard/Orders.jsx
@@ -1,86 +1,184 @@
-import * as React from 'react';
-import Link from '@mui/material/Link';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Title from './Title';
+import * as React from "react";
+import { useState } from "react";
+import Link from "@mui/material/Link";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import Tooltip from "@mui/material/Tooltip";
+import { green } from "@mui/material/colors";
+import { IconButton } from "@mui/material";
+import Title from "./Title";
 
 // Generate Order Data
-function createData(id, date, name, shipTo, paymentMethod, amount) {
-  return { id, date, name, shipTo, paymentMethod, amount };
+function createData(
+	id,
+	date,
+	name,
+  number,
+	shipTo,
+	paymentMethod,
+	status,
+	amount,
+	category,
+) {
+	return { id, date, name,number, shipTo, paymentMethod, status, amount, category };
 }
 
-const rows = [
-  createData(
-    0,
-    '16 Mar, 2019',
-    'Elvis Presley',
-    'Tupelo, MS',
-    'VISA ⠀•••• 3719',
-    312.44,
-  ),
-  createData(
-    1,
-    '16 Mar, 2019',
-    'Paul McCartney',
-    'London, UK',
-    'VISA ⠀•••• 2574',
-    866.99,
-  ),
-  createData(2, '16 Mar, 2019', 'Tom Scholz', 'Boston, MA', 'MC ⠀•••• 1253', 100.81),
-  createData(
-    3,
-    '16 Mar, 2019',
-    'Michael Jackson',
-    'Gary, IN',
-    'AMEX ⠀•••• 2000',
-    654.39,
-  ),
-  createData(
-    4,
-    '15 Mar, 2019',
-    'Bruce Springsteen',
-    'Long Branch, NJ',
-    'VISA ⠀•••• 5919',
-    212.79,
-  ),
+const users = [
+	createData(
+		0,
+		"16 Mar, 2019",
+		"Elvis Presley",
+    "+998 93 612 12 12",
+		"Tupelo, MS",
+		"VISA ⠀•••• 3719",
+		"status",
+		312.44,
+		"New",
+	),
+	createData(
+		1,
+		"16 Mar, 2019",
+		"Paul McCartney",
+    "+998 93 612 12 12",
+		"London, UK",
+		"VISA ⠀•••• 2574",
+		"status",
+		866.99,
+		"Done",
+	),
+	createData(
+		2,
+		"16 Mar, 2019",
+		"Tom Scholz",
+    "+998 93 612 12 12",
+		"Boston, MA",
+		"MC ⠀•••• 1253",
+		"status",
+		100.81,
+		"canceled",
+	),
+	createData(
+		3,
+		"16 Mar, 2019",
+		"Michael Jackson",
+    "+998 93 612 12 12",
+		"Gary, IN",
+		"AMEX ⠀•••• 2000",
+		"status",
+		654.39,
+		"New",
+	),
+	createData(
+		4,
+		"15 Mar, 2019",
+		"Bruce Springsteen",
+    "+998 93 612 12 12",
+		"Long Branch, NJ",
+		"VISA ⠀•••• 5919",
+		"status",
+		212.79,
+		"Done",
+	),
 ];
-
+// console.log(rows);
 function preventDefault(event) {
-  event.preventDefault();
+	event.preventDefault();
 }
 
 export default function Orders() {
-  return (
-    <React.Fragment>
-      <Title>Recent Orders</Title>
-      <Table size="small">
-        <TableHead>
-          <TableRow>
-            <TableCell>Date</TableCell>
-            <TableCell>Name</TableCell>
-            <TableCell>Ship To</TableCell>
-            <TableCell>Payment Method</TableCell>
-            <TableCell align="right">Sale Amount</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.id}>
-              <TableCell>{row.date}</TableCell>
-              <TableCell>{row.name}</TableCell>
-              <TableCell>{row.shipTo}</TableCell>
-              <TableCell>{row.paymentMethod}</TableCell>
-              <TableCell align="right">{`$${row.amount}`}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-      <Link color="primary" href="#" onClick={preventDefault} sx={{ mt: 3 }}>
-        See more orders
-      </Link>
-    </React.Fragment>
-  );
+	const rows = [];
+	const [user, setUser] = useState("All");
+	// console.log(user);
+
+	function funOrder(user) {
+		if (user == "All") {
+			rows.push(...users);
+		} else {
+			users.forEach((item) => {
+				if (item.category == user) {
+					rows.push(item);
+				}
+			});
+		}
+	}
+	funOrder(user);
+
+	return (
+		<React.Fragment>
+			<Stack
+				onClick={(e) => {
+					setUser(e.target.textContent);
+				}}
+				spacing={2}
+				direction="row"
+			>
+				<Button fullWidth variant="contained">
+					New
+				</Button>
+				<Button fullWidth variant="contained" color="success">
+					Done
+				</Button>
+				<Button fullWidth variant="contained" color="error">
+					canceled
+				</Button>
+				<Button fullWidth variant="contained" color="secondary">
+					All
+				</Button>
+			</Stack>
+
+			<Title>Recent Orders</Title>
+			<Table size="small">
+				<TableHead>
+					<TableRow>
+						<TableCell>Date</TableCell>
+						<TableCell>Name</TableCell>
+						<TableCell>Phone number</TableCell>
+						<TableCell>Ship To</TableCell>
+						<TableCell>Payment Method</TableCell>
+						<TableCell>Status</TableCell>
+						<TableCell align="right">Sale Amount</TableCell>
+						<TableCell>Edit</TableCell>
+						<TableCell>Delete</TableCell>
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{rows.map((row) => (
+						<TableRow key={row.id}>
+							<TableCell>{row.date}</TableCell>
+							<TableCell>{row.name}</TableCell>
+							<TableCell>{row.number}</TableCell>
+							<TableCell>{row.shipTo}</TableCell>
+							<TableCell>{row.paymentMethod}</TableCell>
+							<TableCell>{row.status}</TableCell>
+							<TableCell align="right">{`$${row.amount}`}</TableCell>
+							<TableCell>
+								<Tooltip title="edit">
+									<IconButton>
+										<EditIcon sx={{ color: green[500] }} />
+									</IconButton>
+								</Tooltip>
+							</TableCell>
+							<TableCell>
+								<Tooltip title="delete">
+									<IconButton>
+										<DeleteForeverIcon color="error" />
+									</IconButton>
+								</Tooltip>
+							</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
+			<Link color="primary" href="#" onClick={preventDefault} sx={{ mt: 3 }}>
+				See more orders
+			</Link>
+		</React.Fragment>
+	);
 }
