944fa8489e3105dff258349d291d681808f8449c
Abrorbek abdulkhamidov <abdulkhamidovabrorbek@gmail.com>
2023-06-21 14:33:47 +0500

    Map updated

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -84,7 +84,6 @@ function MapWrapper() {
 			scrollWheelZoom={false}
 		>
 			<TileLayer
-				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 			/>
 			<DraggableMarker />
--- src/components/Map/tileLayer.jsx
+++ src/components/Map/tileLayer.jsx
@@ -1,5 +1,4 @@
 const tileLayer = {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
     url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
   }
   
